26c1be65291bc8e64d83eaebfe36172576c8b746
JeongTaekCho <cjt3591@gmail.com>
2026-09-23 15:35:04 +0900

    chore: 프로바이더 셋팅

---
+++ app/RootLayoutProvider.tsx
@@ -0,0 +1,26 @@
+"use client"; // 클라이언트 컴포넌트 선언 필수
+
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function RootLayoutProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 5,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import RootLayoutProvider from "@/app/RootLayoutProvider";
+import Header from "@/src/shared/components/Header";
 
 export const metadata: Metadata = {
   title: "지각 위기 탐지",
@@ -7,10 +9,19 @@ export const metadata: Metadata = {
     "아침 루틴 등을 기록하고 실시간 대중교통 상황을 연동하여 지각하지 않도록 도와주는 서비스입니다.",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body>
+        <RootLayoutProvider>
+          <Header />
+          {children}
+        </RootLayoutProvider>
+      </body>
     </html>
   );
 }
