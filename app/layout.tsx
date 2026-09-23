3dc61464a890ef2c61dc7cae8fed1e7a59fd6aee
JeongTaekCho <cjt3591@gmail.com>
2026-09-23 10:04:52 +0900

    chore: 프로젝트명 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "AI 페이스메이커",
-  description: "면접 발표 교정",
+  title: "지각 위기 탐지",
+  description:
+    "아침 루틴 등을 기록하고 실시간 대중교통 상황을 연동하여 지각하지 않도록 도와주는 서비스입니다.",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
